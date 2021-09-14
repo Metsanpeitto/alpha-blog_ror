73e9f9d4185f8322df7011977a087c6ddd731fad
Metsanpeitto <abraham.asturies@gmail.com>
2021-09-14 16:23:35 +0200

    Add edit & update features and views

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -11,6 +11,11 @@ class ArticlesController < ApplicationController
     def new
         @article =  Article.new
     end
+
+    def edit
+        @article = Article.find(params[:id])   
+    end
+
     
     def create
         @article = Article.new(params.require(:article).permit(:title, :description))
@@ -20,6 +25,17 @@ class ArticlesController < ApplicationController
         else
           render 'new'
         end
-    end 
+    end
+    
+
+    def update
+        @article = Article.find(params[:id])
+        if @article.update(params.require(:article).permit(:title, :description))
+            flash[:notice] = "Article was updated successfully"
+            redirect_top @article
+        else
+            render 'edit'
+        end    
+    end
 
 end        
