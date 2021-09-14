bcf1c6e19e76b45455ca01e32c6a90b4de817210
Metsanpeitto <abraham.asturies@gmail.com>
2021-09-15 00:57:30 +0200

    Add signup and edit account

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_09_14_184955) do
+ActiveRecord::Schema.define(version: 2021_09_14_212354) do
 
   create_table "articles", force: :cascade do |t|
     t.string "title"
@@ -22,8 +22,10 @@ ActiveRecord::Schema.define(version: 2021_09_14_184955) do
 
   create_table "users", force: :cascade do |t|
     t.string "username"
+    t.string "email"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "password_digest"
   end
 
 end
