fb90628cc8d17e52c5f0e0ba380400a0183af39b
Metsanpeitto <abraham.asturies@gmail.com>
2021-09-14 16:05:17 +0200

    Add notice message to article.create

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,11 +1,25 @@
 class ArticlesController < ApplicationController
-
+    
     def show
-        byebug
         @article = Article.find(params[:id])
     end 
     
     def index    
        @articles = Article.all
-    end    
+    end 
+    
+    def new
+        @article =  Article.new
+    end
+    
+    def create
+        @article = Article.new(params.require(:article).permit(:title, :description))
+        if @article.save
+          flash[:notice] = "Article was created successfully."  
+          redirect_to article_path(@article)
+        else
+          render 'new'
+        end
+    end 
+
 end        
